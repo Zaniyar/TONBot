9689d41c37ba3b3884ee2f558a82a6e167fbedb0
CHOIX <jaha@zhaw.ch>
2024-02-06 20:32:32 +0100

    no webxr

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -57,7 +57,7 @@ export function Counter() {
             </FlexBoxCol>
           </FlexBoxRow>
           <FlexBoxRow>
-            {JSON.stringify(window.navigator)}
+            window: {JSON.stringify(window)}
           </FlexBoxRow>
           <FlexBoxRow>
             <XRButton
